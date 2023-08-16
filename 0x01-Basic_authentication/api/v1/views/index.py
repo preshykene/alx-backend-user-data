5dc77a263a707cd54ffbde708b37891d0329557e
preshykene <preciousekene51@gmail.com>
2023-08-16 06:46:58 +0000

    Done with this

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,15 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized/', strict_slashes=False)
+def unauthorized() -> str:
+    """unauthorized function"""
+    abort(401)
+
+
+@app_views.route('/forbidden/', strict_slashes=False)
+def forbidden() -> str:
+    """forbidden function"""
+    abort(403)
